1cf6eb40d18f9d743dc10f1eea92472e140b4445
javlakelly <kbrofall@gmail.com>
2013-10-24 11:23:00 +0200

    rework, again!

--- Rework/js/main.js
+++ Rework/js/main.js
@@ -10,7 +10,27 @@ $(document).ready(function(){
     $('#settings').fadeIn();
   });
 
+    // Function that is called once the data is returned
+    function processImages(response) {
+    // The variable "data" represents the information we got back.
+    //find the image url: data.items[i].media.m
+    //find the amount of loops: data.items.length
+    	for (var i=0; i < response.data.length; i++) {
+    		var url = response.data[i].images.low_resolution.url;
+    		$("#images").append("<img src='" + url + "'>");
+    		//    <img src="someadress.jpg">
+    		//    <img src='someadress.jpg'>
+   		}
+  	}
 
+	$('#search').click(function(){
+		var hashtag = $('#hashtag').val();
+		var url = "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?access_token=22702850.f59def8.6825c67f29e84bf8826b9cdd204743ed&callback=?";
+		$.getJSON(url, processImages);
+      $("#images img").remove();
+  
+
+});
 
 
 
