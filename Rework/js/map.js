3c6458ba9b74ccc63f68da842a150d2a009ebb22
pushmatrix <daniel.beauchamp@gmail.com>
2013-10-29 03:30:54 +0000

    Added showing properties on the map.

--- Rework/js/map.js
+++ Rework/js/map.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	//var map = L.mapbox.map('map', 'tastyplanetse.map-mnphjjx4');
 	//var json = null;
-	var map_options, map, marker, lat, lon;
-	
+	map = null;
+	var map_options, marker, lat, lon;
+	var geocoder = new google.maps.Geocoder();
+  var currentInfoWin = null;
 
 
 
@@ -27,10 +29,8 @@ $(document).ready(function(){
 	var placeListRef = new Firebase("https://tastyplanetse.firebaseio.com/dinners")
 	placeListRef.on('child_added', function(snapshot){
 		var placeData = snapshot.val();
+		var id = snapshot.name();
 		var userAddress = placeData.userAddress;
-
-		lat = userAddress.substring(0, userAddress.indexOf(","));
-		lon = userAddress.substring(userAddress.indexOf(",") + 1, userAddress.length);
 
 		console.log("user is here: " + placeData.userAddress);
 
@@ -38,19 +38,50 @@ $(document).ready(function(){
 		//console.log("latitude: " + lat);
 		//console.log("longitude: " + lon);
 		//console.log(properties.userAddress);
-		createMarker(lat, lon);
+		createMarker(id, placeData);
 	});
+
+
+  window.codeAddress = function(address, callback) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        callback(null, results[0].geometry.location);
+      } else {
+        callback("uh oh something went wrong with geocoding your address. Make sure your address actually exists :)");
+      }
+    });
+  }   
 
 	
 
-	function createMarker(lat, lon){
-		var myLatLng = new google.maps.LatLng(lat, lon);
-		Marker = new google.maps.Marker({
+	function createMarker(id, properties){
+		var myLatLng = new google.maps.LatLng(properties.lat, properties.lng);
+		var marker = new google.maps.Marker({
 			position: myLatLng,
 			map: map
-		}
+		});
 
-	);
+		var contentString = '<div class="infowindow">'+
+		  '<h1>' + properties.dinnerName + '</h1>' +
+			'<div>User: ' + properties.userName + '</div>' +
+			'<div>Where: ' + properties.address + '</div>' +
+			'<div>When: ' + properties.dateTime + '</div>' +
+			'<div>Max Seats: ' + properties.maxSeats + '</div>' +
+			'<div>Message: ' + properties.message + '</div>' +
+			'<button data-id="' + id + '"class="bookListingBtn">Book</button>' +
+      '</div>';
+
+		var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+  	google.maps.event.addListener(marker, 'click', function() {
+    	infowindow.open(map,marker);
+      if (currentInfoWin !== null) {
+        currentInfoWin.close(map, this); 
+      }
+      currentInfoWin = infowindow;  
+  	});
 	}
 
 
